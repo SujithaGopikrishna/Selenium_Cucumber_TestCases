6a575802f3d8a2eccddc547a21a736ca283f1274
Sujitha <63647569+SujithaGopikrishna@users.noreply.github.com>
2020-08-27 16:07:16 +0530

    Delete Ajio.java

--- java/runner/Ajio.java
+++
@@ -1,9 +0,0 @@
-package runner;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.SnippetType;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(features = "D:\\sujiselenium\\Selenium\\Cucumber\\src\\main\\java\\feature\\ajio.feature", glue="steps", monochrome= true, dryRun=true, 
-snippets = SnippetType.CAMELCASE)
-public class Ajio extends AbstractTestNGCucumberTests{}
